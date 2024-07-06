82cadf49b1da18f1b01cad6329ac5af20ea10b05
Currypan1229 <abcqqqwpvp@gmail.com>
2024-07-07 05:50:44 +0900

    コメントを削除し、可読性向上

--- module/1_17_to_1_20/src/main/java/net/akazukin/mapart/module/m1_17_to_1_20/MapartChunkGenerator.java
+++ module/1_17_to_1_20/src/main/java/net/akazukin/mapart/module/m1_17_to_1_20/MapartChunkGenerator.java
@@ -17,24 +17,23 @@ public class MapartChunkGenerator extends ChunkGenerator {
     }
 
     @Override
-    public void generateBedrock(final WorldInfo worldInfo, @Nonnull final Random random,
-                                final int chunkX, final int chunkZ, final ChunkData chunkData) {
-
-        final int offsetX = (int) ((chunkX + 4) % (this.mgr.getSize() * MapartManager.MAP_SIZE));
-        final int offsetZ = (int) ((chunkZ + 4) % (this.mgr.getSize() * MapartManager.MAP_SIZE));
+    public void generateBedrock(@Nonnull final WorldInfo worldInfo, @Nonnull final Random random,
+                                final int chunkX, final int chunkZ, @Nonnull final ChunkData chunkData) {
+        final int chuOffX = (int) ((chunkX + 4) % (this.mgr.getSize() * MapartManager.MAP_SIZE));
+        final int chuOffZ = (int) ((chunkZ + 4) % (this.mgr.getSize() * MapartManager.MAP_SIZE));
 
         for (int x = 0; x < 16; x++) {
             for (int z = 0; z < 16; z++) {
                 if (x == 0 || x == 15 || z == 0 || z == 15) {
-                    if (this.mgr.getSize() >= 16 && ((x == 0 && offsetX % (8 * 16) == 0) || (z == 0 && offsetZ % (8 * 16) == 0) || (x == 15 && (offsetX + 1) % (8 * 16) == 0) || (z == 15 && (offsetZ + 1) % (8 * 16) == 0))) {
+                    if (this.mgr.getSize() >= 16 && ((x == 0 && chuOffX % (8 * 16) == 0) || (z == 0 && chuOffZ % (8 * 16) == 0) || (x == 15 && (chuOffX + 1) % (8 * 16) == 0) || (z == 15 && (chuOffZ + 1) % (8 * 16) == 0))) {
                         chunkData.setBlock(x, chunkData.getMinHeight(), z, Material.RED_WOOL);
-                    } else if (this.mgr.getSize() >= 8 && ((x == 0 && offsetX % (8 * 8) == 0) || (z == 0 && offsetZ % (8 * 8) == 0) || (x == 15 && (offsetX + 1) % (8 * 8) == 0) || (z == 15 && (offsetZ + 1) % (8 * 8) == 0))) {
+                    } else if (this.mgr.getSize() >= 8 && ((x == 0 && chuOffX % (8 * 8) == 0) || (z == 0 && chuOffZ % (8 * 8) == 0) || (x == 15 && (chuOffX + 1) % (8 * 8) == 0) || (z == 15 && (chuOffZ + 1) % (8 * 8) == 0))) {
                         chunkData.setBlock(x, chunkData.getMinHeight(), z, Material.RED_WOOL);
-                    } else if (this.mgr.getSize() >= 4 && ((x == 0 && offsetX % (8 * 4) == 0) || (z == 0 && offsetZ % (8 * 4) == 0) || (x == 15 && (offsetX + 1) % (8 * 4) == 0) || (z == 15 && (offsetZ + 1) % (8 * 4) == 0))) {
+                    } else if (this.mgr.getSize() >= 4 && ((x == 0 && chuOffX % (8 * 4) == 0) || (z == 0 && chuOffZ % (8 * 4) == 0) || (x == 15 && (chuOffX + 1) % (8 * 4) == 0) || (z == 15 && (chuOffZ + 1) % (8 * 4) == 0))) {
                         chunkData.setBlock(x, chunkData.getMinHeight(), z, Material.ORANGE_WOOL);
-                    } else if (this.mgr.getSize() >= 2 && ((x == 0 && offsetX % (8 * 2) == 0) || (z == 0 && offsetZ % (8 * 2) == 0) || (x == 15 && (offsetX + 1) % (8 * 2) == 0) || (z == 15 && (offsetZ + 1) % (8 * 2) == 0))) {
+                    } else if (this.mgr.getSize() >= 2 && ((x == 0 && chuOffX % (8 * 2) == 0) || (z == 0 && chuOffZ % (8 * 2) == 0) || (x == 15 && (chuOffX + 1) % (8 * 2) == 0) || (z == 15 && (chuOffZ + 1) % (8 * 2) == 0))) {
                         chunkData.setBlock(x, chunkData.getMinHeight(), z, Material.YELLOW_WOOL);
-                    } else if ((x == 0 && offsetX % 8 == 0) || (z == 0 && offsetZ % 8 == 0) || (x == 15 && (offsetX + 1) % 8 == 0) || (z == 15 && (offsetZ + 1) % 8 == 0)) {
+                    } else if ((x == 0 && chuOffX % 8 == 0) || (z == 0 && chuOffZ % 8 == 0) || (x == 15 && (chuOffX + 1) % 8 == 0) || (z == 15 && (chuOffZ + 1) % 8 == 0)) {
                         chunkData.setBlock(x, chunkData.getMinHeight(), z, Material.PINK_WOOL);
                     } else {
                         chunkData.setBlock(x, chunkData.getMinHeight(), z, Material.WHITE_WOOL);
@@ -44,33 +43,6 @@ public class MapartChunkGenerator extends ChunkGenerator {
                 }
             }
         }
-
-        /*for (int x = 0; x < 16; x++) {
-            for (int z = 0; z < 16; z++) {
-                if (x == 0 || x == 15 || z == 0 || z == 15) {
-                    if (this.mgr.getSize() >= 8 && ((x == 0 && (chunkX + 4) % (8 * 8) == 0) || (z == 0 && (chunkZ +
-                    4) % (8 * 8) == 0) || (x == 15 && (chunkX + 5) % (8 * 8) == 0) || (z == 15 && (chunkZ + 5) % (8 *
-                     8) == 0))) {
-                        chunkData.setBlock(x, chunkData.getMinHeight(), z, Material.RED_WOOL);
-                    } else if (this.mgr.getSize() >= 4 && ((x == 0 && (chunkX + 4) % (8 * 4) == 0) || (z == 0 &&
-                    (chunkZ + 4) % (8 * 4) == 0) || (x == 15 && (chunkX + 5) % (8 * 4) == 0) || (z == 15 && (chunkZ +
-                     5) % (8 * 4) == 0))) {
-                        chunkData.setBlock(x, chunkData.getMinHeight(), z, Material.ORANGE_WOOL);
-                    } else if (this.mgr.getSize() >= 2 && ((x == 0 && (chunkX + 4) % (8 * 2) == 0) || (z == 0 &&
-                    (chunkZ + 4) % (8 * 2) == 0) || (x == 15 && (chunkX + 5) % (8 * 2) == 0) || (z == 15 && (chunkZ +
-                     5) % (8 * 2) == 0))) {
-                        chunkData.setBlock(x, chunkData.getMinHeight(), z, Material.YELLOW_WOOL);
-                    } else if ((x == 0 && (chunkX + 4) % 8 == 0) || (z == 0 && (chunkZ + 4) % 8 == 0) || (x == 15 &&
-                    (chunkX + 5) % 8 == 0) || (z == 15 && (chunkZ + 5) % 8 == 0)) {
-                        chunkData.setBlock(x, chunkData.getMinHeight(), z, Material.PINK_WOOL);
-                    } else {
-                        chunkData.setBlock(x, chunkData.getMinHeight(), z, Material.WHITE_WOOL);
-                    }
-                } else {
-                    chunkData.setBlock(x, chunkData.getMinHeight(), z, Material.STONE);
-                }
-            }
-        }*/
     }
 
     @Override
